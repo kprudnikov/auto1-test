d8f20172a9b2d8525882aac41bac9beba5fcd153
Konstantin Prudnikov <konstantin@ebuilders.nl>
2018-04-25 23:47:00 +0300

    feat: added redux-saga

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,14 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import globalSagas from './containers/App/sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = (preloadedState) => {
   const enhancers = [];
+
+  enhancers.push(applyMiddleware(sagaMiddleware));
 
   if (process.env.NODE_ENV === 'development') {
     const devToolsExtension = window.devToolsExtension
@@ -12,13 +18,21 @@ const configureStore = (preloadedState) => {
     }
   }
 
-  return createStore(
+  const store = createStore(
     reducers,
     preloadedState,
     compose(
       ...enhancers
     )
   );
+
+  store.runSaga = sagaMiddleware.run;
+
+  globalSagas().forEach((saga) => {
+    sagaMiddleware.run(saga);
+  });
+
+  return store;
 };
 
 export default configureStore;
